5e78e55cae762c6ef78fc95cd6595744ec33b205
Your Name <abhishekgobindgarhia019@gmail.com>
2025-07-23 15:59:20 +0530

    moore

---
+++ vector/vector/majorityElement(mooreAlgorithm).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n= nums.size();
+      int freq =0;
+      int ans =0;
+      for(int i=0; i<n; i++){
+        if(freq==0){
+            ans= nums[i];
+        }
+        if(ans == nums[i]){
+            freq++;
+        } else{
+            freq--;
+        }
+      }
+      return ans;
+    }
+};
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    Solution sol;
+    int result = sol.majorityElement(nums);
+    cout << "Majority element: " << result << endl;
+    return 0;
+}
