b0fdc87f92868e161418e0fc7f5250ac1acdf9cc
Your Name <abhishekgobindgarhia019@gmail.com>
2025-07-26 23:10:00 +0530

    pascal triangle

---
+++ Array/PascalTriangle.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+ vector<vector<int>> generate(int numRows){
+ 	
+ 	vector<vector<int>> triangle;
+ 	
+ 	for(int i =0; i <numRows; i++){
+ 		vector <int> row(i+1,1);
+ 			for(int j =1; j <i; j++){
+ 				row[j]= triangle[i-1][j-1]+triangle[i-1][j];
+ 				
+	 }
+	  triangle.push_back(row);
+ }
+ return triangle;
+}
+
+ 
+ int main(){
+ 	int rows;
+ 	cout<<"No. of Rows:";
+ 	cin>>rows;
+ 	vector<vector<int>> pascalTriangle = generate(rows);
+ 	for(const auto& row : pascalTriangle){
+ 		for(int num : row){
+ 			cout<<num<<" ";
+ 		}
+ 		cout<<endl;
+ 	}
+ 	return 0;
+    }
