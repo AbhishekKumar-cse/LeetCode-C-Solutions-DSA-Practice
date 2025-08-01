dca87a09c1220dcd5f31e1af6336f282a3f1d8e9
Your Name <abhishekgobindgarhia019@gmail.com>
2025-08-01 23:10:25 +0530

    plus one

---
+++ Math/plusOne.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for(int i = n-1; i>=0; i--){
+            if(digits[i]<9){
+              digits[i]++;
+              return digits;
+            }
+            digits[i]=0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
+int main() {
+    Solution sol;
+    vector<int> digits = {1, 2, 3};
+    vector<int> result = sol.plusOne(digits);
+    for (int i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
