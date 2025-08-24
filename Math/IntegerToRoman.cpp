395ec2b889d75f7cd04f88ffe325c6fd28ac2e3a
Your Name <abhishekgobindgarhia019@gmail.com>
2025-08-24 23:41:39 +0530

    int to roman

---
+++ Math/IntegerToRoman.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+string intToRoman(int num){
+    vector<pair<int, string>> val = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
+        {1, "I"}
+    };
+    string result;
+    for (const auto& [value, symbol] : val) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int num;
+    cout << "Enter an integer: ";
+    cin >> num;
+    cout << "Roman numeral: " << intToRoman(num) << endl;
+    return 0;
+}
